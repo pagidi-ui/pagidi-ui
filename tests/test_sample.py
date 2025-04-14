fa294d7422ce0f73c28220f5daae4d58e5e171b0
pagidi naga srinu <srinivaschowdarypagidis@gmail.com>
2025-04-14 19:21:39 -0400

    Update test_sample.py
    Signed-off-by: pagidi naga srinu <srinivaschowdarypagidis@gmail.com>

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,2 +1,8 @@
-def test_example():
-    assert 1 + 1 == 2   
+import unittest
+
+class TestSample(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
