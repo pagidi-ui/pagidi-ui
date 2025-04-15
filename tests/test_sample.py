bbe3ef217aa1bdefdfdc8105191cf65ff6507dfc
pagidi naga srinu <srinivaschowdarypagidis@gmail.com>
2025-04-15 22:22:22 +0000

    Update test_sample.py with additional test cases

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,5 +4,11 @@ class TestSample(unittest.TestCase):
     def test_example(self):
         self.assertEqual(1 + 1, 2)
 
+    def test_multiplication(self):
+        self.assertEqual(2 * 3, 6)
+
+    def test_division(self):
+        self.assertEqual(10 / 2, 5)
+
 if __name__ == '__main__':
     unittest.main()
